400e47d8ac55b33346cf2e30eb9551e69f3f7d25
tuliocv <130805106+tuliocv@users.noreply.github.com>
2025-10-31 18:18:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 import io
 
 st.set_page_config(page_title="Removedor de Fundo", page_icon="🪄")
-st.title("🪄 Removedor de Fundo — Simples e Rápido")
+st.title("🪄 Removedor de Fundo")
 
 uploaded_file = st.file_uploader("Envie uma imagem (PNG, JPG, JPEG):", type=["png", "jpg", "jpeg"])
 
@@ -12,10 +12,10 @@ if uploaded_file:
     image = Image.open(uploaded_file).convert("RGBA")
     st.image(image, caption="📷 Imagem Original", width="stretch")
 
-    with st.spinner("Removendo fundo... ⏳"):
+    with st.spinner("Removendo fundo... ⏳ Vai pegar um cafezinho que dá tempo 😃"):
         output = remove(image)
 
-    st.success("✅ Fundo removido com sucesso!")
+    st.success("✅ Fundo removido com sucesso! Viu? Foi muito rápido!")
     st.image(output, caption="🪄 Imagem sem fundo", width="stretch")
 
     buf = io.BytesIO()
